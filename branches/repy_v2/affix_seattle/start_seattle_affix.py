989e57555929829d5ba8f4a113c8ae6953dfffcb
Name <USER@DOMAIN>
2014-02-18 03:42:56 +0000

    Updated some Affix files in preparation to fix for #1329

--- branches/repy_v2/affix_seattle/start_seattle_affix.py
+++ branches/repy_v2/affix_seattle/start_seattle_affix.py
@@ -1,22 +1,47 @@
+"""
+<Program Name>
+  start_seattle_affix.py
+
+<Purpose>
+  This is a very simple script that uses the Seattle
+  advertise service to enable Affixes for Seattle 
+  nodemanager and defines what Affixes they should
+  use.
+"""
+
 from repyportability import *
 _context = locals()
 add_dy_support(_context)
 
 dy_import_module_symbols("advertise.repy")
 
+affix_stack_key = "SeattleAffixStack"
+enable_affix_key = "EnableSeattleAffix"
+
+# Define the Affix stack.
 try:
-    advertise_announce("SeattleAffixStack", "(NoopShim)", 600)
+    advertise_announce(affix_stack_key, "(NoopShim)", 600)
 except AdvertiseError, e:
     print "Advertise error: " + str(e)
 
-lookup_string = advertise_lookup("SeattleAffixStack")
-print "Current lookup string is: " + str(lookup_string)
-
+# Check what the flag is set to.
 try:
-    advertise_announce("EnableSeattleAffix", "True", 600)
+    lookup_string = advertise_lookup(affix_stack_key)
+except (AdvertiseError, TimeoutError):
+    print "Unable to lookup key '%s' due to advertise error." % affix_stack_key
+else:
+    print "Current lookup string is: " + str(lookup_string)
+
+# Enable Affixes for Seattle.
+try:
+    advertise_announce(enable_affix_key, "True", 600)
 except AdvertiseError, e:
     print "Advertise error: " + str(e)
 
-
-lookup_string = advertise_lookup("EnableSeattleAffix")
-print "EnableSeattleAffix flag set to: " + str(lookup_string)
+# Check what the Affix flag is set to.
+try:
+    lookup_string = advertise_lookup(enable_affix_key)
+except (AdvertiseError, TimeoutError):
+    print "Unable to lookup key '%s' due to advertise error." % enable_affix_key
+else:    
+    print "EnableSeattleAffix flag set to: " + str(lookup_string)
--- branches/repy_v2/affix_seattle/stop_seattle_affix.py
+++ branches/repy_v2/affix_seattle/stop_seattle_affix.py
@@ -1,22 +1,47 @@
+"""
+<Program Name>
+  stop_seattle_affix.py
+
+<Purpose>
+  This is a very simple script that uses the Seattle
+  advertise service to disable Affixes for Seattle 
+  nodemanager and defines what Affixes they should
+  use.
+"""
+
 from repyportability import *
 _context = locals()
 add_dy_support(_context)
 
 dy_import_module_symbols("advertise.repy")
 
+affix_stack_key = "SeattleAffixStack"
+enable_affix_key = "EnableSeattleAffix"
+
 try:
-    advertise_announce("SeattleAffixStack", "(NoopShim)", 600)
+    advertise_announce(affix_stack_key, "(NoopShim)", 600)
 except AdvertiseError, e:
     print "Advertise error: " + str(e)
 
-lookup_string = advertise_lookup("SeattleAffixStack")
-print "Current lookup string is: " + str(lookup_string)
-
+# Check what the flag is set to.
 try:
-    advertise_announce("EnableSeattleAffix", "False", 600)
+    lookup_string = advertise_lookup(affix_stack_key)
+except (AdvertiseError, TimeoutError):
+    print "Unable to lookup key '%s' due to advertise error." % affix_stack_key
+else:
+    print "Current lookup string is: " + str(lookup_string)
+
+# Disable Affixes for Seattle.
+try:
+    advertise_announce(enable_affix_key, "False", 600)
 except AdvertiseError, e:
     print "Advertise error: " + str(e)
 
+# Check what the Affix flag is set to.
+try:
+    lookup_string = advertise_lookup(enable_affix_key)
+except (AdvertiseError, TimeoutError):
+    print "Unable to lookup key '%s' due to advertise error." % enable_affix_key
+else:    
+    print "EnableSeattleAffix flag set to: " + str(lookup_string)
 
-lookup_string = advertise_lookup("EnableSeattleAffix")
-print "EnableSeattleAffix flag set to: " + str(lookup_string)
